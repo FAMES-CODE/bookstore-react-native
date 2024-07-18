af88440c4ff263e0e5157df273b5157294febadf
Amine Ferkani <74460480+FAMES-CODE@users.noreply.github.com>
2024-07-18 10:58:59 +0200

    modified:   app/index.tsx Added warning if app is running on web

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,19 @@
 import { View } from "react-native";
 import styles from "../styles/style.js";
-import { IconButton, Text } from "react-native-paper";
+import {
+  IconButton,
+  Text,
+  Dialog,
+  Portal,
+  PaperProvider,
+  Button,
+} from "react-native-paper";
 import { Link } from "expo-router";
-
+import { Platform } from "react-native";
+import React from "react";
 export default function Index() {
+  const [visible, setVisible] = React.useState(true);
+
   return (
     <View
       style={[
@@ -17,6 +27,9 @@ export default function Index() {
         },
       ]}
     >
+      {Platform.OS === "web" ? (
+        <WebAlert visible={visible} setVisible={setVisible} />
+      ) : null}
       <IconButton
         icon="book"
         iconColor="black"
@@ -50,6 +63,44 @@ export default function Index() {
           Discover our books
         </Link>
       </Text>
+    </View>
+  );
+}
+
+function WebAlert({ visible, setVisible }) {
+  return (
+    <View
+      style={{
+        display: visible ? "flex" : "none",
+        position: "relative",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "red",
+        padding: 20,
+        borderRadius: 20,
+      }}
+    >
+      <Text variant="titleLarge" style={styles.h1}>
+        You're running the app on the web!.
+      </Text>
+      <Text variant="titleLarge" style={styles.h1}>
+        The UI is developed and designed for mobile.
+      </Text>
+      <Text variant="titleLarge" style={styles.h1}>
+        Bugs expected.
+      </Text>
+      <Button
+        style={{
+          position: "absolute",
+          right: 0,
+          bottom: 0,
+        }}
+        textColor="white"
+        onPress={() => setVisible(false)}
+      >
+        Hide
+      </Button>
     </View>
   );
 }

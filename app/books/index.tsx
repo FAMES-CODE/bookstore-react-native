535642bf0bb974c1611df22458ea436b3998edeb
Amine Ferkani <74460480+FAMES-CODE@users.noreply.github.com>
2024-07-18 10:16:36 +0200

    modified:   app/books/index.tsx Used book component 	modified:   components/books/Book.tsx Completed book component 	modified:   contexts/cart.tsx Added types

--- app/books/index.tsx
+++ app/books/index.tsx
@@ -5,7 +5,8 @@ import styles from "@/styles/style";
 import { Button, Text } from "react-native-paper";
 import books from "../../Data/books";
 import useCart from "@/hooks/useCart";
-import { images } from "../../Data/images";
+import Book from "@/components/books/Book";
+
 export default function Index() {
   if (!books || books.length === 0) {
     return (
@@ -49,75 +50,3 @@ export default function Index() {
   );
 }
 
-function Book({
-  props,
-}: {
-  props: { image: keyof typeof images; title: string; price: number };
-}) {
-  if (!props || !props.image || !props.title || !props.price) {
-    throw new Error("Invalid Book props");
-  }
-  const { cart, addToCart, updateQuantity } = useCart();
-  return (
-    <View
-      style={{
-        marginBottom: 60,
-        width: "100%",
-        height: 500,
-      }}
-    >
-      <Image
-        source={images[props.image]}
-        style={{
-          width: "100%",
-          height: "100%",
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
-        }}
-        contentFit="contain"
-      />
-      <View
-        style={{
-          display: "flex",
-          width: "auto",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#A0937D",
-          borderBottomLeftRadius: 20,
-          borderBottomRightRadius: 20,
-        }}
-      >
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            width: "100%",
-            paddingLeft: 20,
-            paddingRight: 20,
-          }}
-        >
-          <Text style={[styles.text]}>{props.title}</Text>
-          <Text style={[styles.text]}>{props.price}$</Text>
-        </View>
-        <Button
-          icon="cart"
-          mode="text"
-          textColor="white"
-          onPress={() => {
-            const bookInCart = cart.find((book: any) => book.id === props.id);
-            const quantity = bookInCart ? bookInCart.quantity + 1 : 1;
-            if (bookInCart) {
-              updateQuantity(props.id, quantity);
-            } else {
-              addToCart({ ...props, quantity });
-            }
-          }}
-        >
-          Add to cart
-        </Button>
-      </View>
-    </View>
-  );
-}
--- contexts/cart.tsx
+++ contexts/cart.tsx
@@ -1,10 +1,11 @@
+import { images } from '@/Data/images';
 import { createContext } from 'react';
 
 const CartContext = createContext({
 	cart: [],
-	addToCart: () => {},
+	addToCart: (p0: { quantity: any; id: number | null | undefined; image: keyof images; title: string; price: number; }) => {},
 	removeFromCart: () => {},
-	updateQuantity: () => {},
+	updateQuantity: (id: number | null | undefined, quantity: any) => {},
 	clearCart: () => {},
 });
 

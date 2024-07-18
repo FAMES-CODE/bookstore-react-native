53fae1d3c87153d834123ebe6d3a0b6d19e29b4a
Amine Ferkani <74460480+FAMES-CODE@users.noreply.github.com>
2024-07-18 04:46:09 +0200

    modified:   app/checkout/index.tsx Added form component and bug fix     modified:   components/checkout/Form.tsx Added form component

--- app/checkout/index.tsx
+++ app/checkout/index.tsx
@@ -4,11 +4,14 @@ import { Text } from "react-native-paper";
 import styles from "@/styles/style";
 import useCart from "@/hooks/useCart";
 import Item from "@/components/checkout/Item";
+import { useRouter } from "expo-router";
+import Form from "@/components/checkout/Form";
  
 
 export default function Index() {
   const { cart } = useCart();
-  console.log(cart);
+  const router = useRouter();
+  if(cart.length == 0) return router.back() 
   return (
     <View style={[styles.view]}>
       <Text
@@ -28,6 +31,8 @@ export default function Index() {
         {cart && cart.length > 0
           ? cart.map((item, index) => <Item key={index} props={item} />)
           : "No items in cart"}
+
+          <Form />
       </View>
     </View>
   );
--- components/checkout/Form.tsx
+++ components/checkout/Form.tsx
@@ -1,11 +1,62 @@
-import { View } from 'react-native'
-import React from 'react'
-import { Text } from 'react-native-paper'
+import { View } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, TextInput } from "react-native-paper";
+import useCart from "@/hooks/useCart";
 
 export default function Form() {
+  const { cart } = useCart();
+  const [form, setForm] = useState({
+    name: "",
+    address: "",
+    phone: "",
+    ...cart,
+  });
   return (
     <View>
-      <Text>Form</Text>
+      <Text style={{ color: "#000" }}>Form</Text>
+      <View>
+        <TextInput
+          style={{
+            backgroundColor: "#E7D4B5",
+            fontWeight: "bold",
+          }}
+          mode="outlined"
+          textColor="black"
+          activeOutlineColor="#B5C18E"
+          outlineStyle={{ borderColor: "#B5C18E" }}
+          placeholder="Enter your name"
+          placeholderTextColor="black"
+          value={form.name}
+          onChangeText={(text) => setForm({ ...form, name: text })}
+        />
+        <TextInput
+          style={{
+            backgroundColor: "#E7D4B5",
+            fontWeight: "bold",
+          }}
+          outlineStyle={{ borderColor: "#B5C18E" }}
+          mode="outlined"
+          textColor="black"
+          placeholder="Enter your address"
+          placeholderTextColor="black"
+          value={form.address}
+          onChangeText={(text) => setForm({ ...form, address: text })}
+        />
+        <TextInput
+          style={{
+            backgroundColor: "#E7D4B5",
+            fontWeight: "bold",
+          }}
+          outlineStyle={{ borderColor: "#B5C18E" }}
+          mode="outlined"
+          textColor="black"
+          placeholder="Enter your phone number"
+          placeholderTextColor="black"
+          value={form.phone}
+          onChangeText={(text) => setForm({ ...form, phone: text })}
+        />
+        <Button mode="contained" onPress={() => console.log(form)}>Validate</Button>
+      </View>
     </View>
-  )
+  );
 }
